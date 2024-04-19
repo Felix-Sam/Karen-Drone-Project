b2aee04545cf80983404b15d29b6712404f1d4c4
felix sam <felixsam922@gmail.com>
2024-04-19 18:53:19 +0100

    First version

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ from inference import get_model
 import supervision as sv
 
 
-model = get_model(model_id="drone_07/1")
-
+model_drone = get_model(model_id="drone_07/1")
+model_birds = get_model(model_id="birds-scarecrow/6")
 
 with st.sidebar:
     st.title('AI Zend MVP Project')
     st.image('logo.jpeg')
-    options = st.radio('SELECT',['drones','birds'])
+    options = st.radio(':red[_SELECT_]',['drones','birds'])
     st.title('About this app')
     st.markdown("This app helps you detect birds or drones by uploading an image of it.")
 
@@ -23,9 +23,9 @@ if options == 'drones':
     if image:
         img = Image.open(image).convert("RGB")
         img_array = np.array(img)
-        results = model.infer(img_array)
+        results = model_drone.infer(img_array)
         detections = sv.Detections.from_inference(results[0].dict(by_alias=True, exclude_none=True))
-        bounding_box_annotator = sv.BoundingBoxAnnotator(thickness=8)
+        bounding_box_annotator = sv.BoundingBoxAnnotator(thickness=5)
         label_annotator = sv.LabelAnnotator(text_thickness=5,text_scale=4)
 
         annotated_image = bounding_box_annotator.annotate(
@@ -34,6 +34,26 @@ if options == 'drones':
         scene=annotated_image, detections=detections)
         st.image(annotated_image,use_column_width=True)
         st.subheader(':red[_Drone_ Description Coming Soon :sunglasses:]')
+        
+
+if options == 'birds':
+    st.header(f':red[{options.upper()} SELECTED]')
+    st.image('bird.jpeg')
+    image = st.file_uploader("Take a picture")
+    if image:
+        img = Image.open(image).convert("RGB")
+        img_array = np.array(img)
+        results = model_birds.infer(img_array)
+        detections = sv.Detections.from_inference(results[0].dict(by_alias=True, exclude_none=True))
+        bounding_box_annotator = sv.BoundingBoxAnnotator(thickness=5)
+        label_annotator = sv.LabelAnnotator(text_thickness=5,text_scale=4)
+
+        annotated_image = bounding_box_annotator.annotate(
+        scene=img_array, detections=detections)
+        annotated_image = label_annotator.annotate(
+        scene=annotated_image, detections=detections)
+        st.image(annotated_image,use_column_width=True)
+        st.subheader(':red[_Bird_ Description Coming Soon :sunglasses:]')
         
 
 
